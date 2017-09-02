0ff2990ddcb36e51669125a4abb3354d52852d7d
Karanum <hyougakazu@gmail.com>
2017-09-02 11:51:36 +0200

    Backported API 6 changes into API 5

--- src/main/java/com/karanumcoding/adamantineshield/listeners/LiquidFlowListener.java
+++ src/main/java/com/karanumcoding/adamantineshield/listeners/LiquidFlowListener.java
@@ -11,6 +11,8 @@ import org.spongepowered.api.data.property.block.MatterProperty.Matter;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.Order;
 import org.spongepowered.api.event.block.ChangeBlockEvent;
+import org.spongepowered.api.world.Location;
+import org.spongepowered.api.world.World;
 
 import com.karanumcoding.adamantineshield.db.Database;
 import com.karanumcoding.adamantineshield.db.queue.BlockQueueEntry;
@@ -26,7 +28,11 @@ public class LiquidFlowListener {
 	
 	@Listener(order = Order.POST)
 	public void onLiquidFlow(ChangeBlockEvent.Pre e) {
-		BlockSnapshot snapshot = e.getTargetWorld().createSnapshot(e.getLocations().get(0).getBlockPosition());
+		if (e.getLocations().isEmpty()) return;
+				
+		Location<World> loc = e.getLocations().get(0);
+		BlockSnapshot snapshot = loc.getExtent().createSnapshot(loc.getBlockPosition());
+		
 		Optional<MatterProperty> matter = snapshot.getState().getProperty(MatterProperty.class);
 		if (matter.isPresent() && matter.get().getValue() == Matter.LIQUID) {
 			String name = "Water";
