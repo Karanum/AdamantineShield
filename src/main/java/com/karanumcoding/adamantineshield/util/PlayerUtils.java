73a6e98818d96b40badd25d25f5452cc853c27e9
Karanum <hyougakazu@gmail.com>
2017-09-19 10:29:53 +0200

    Changed player lookup to be more reliable

--- src/main/java/com/karanumcoding/adamantineshield/util/PlayerUtils.java
+++ src/main/java/com/karanumcoding/adamantineshield/util/PlayerUtils.java
@@ -4,17 +4,28 @@ import java.util.Optional;
 import java.util.UUID;
 
 import org.spongepowered.api.Sponge;
-import org.spongepowered.api.profile.GameProfile;
+import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.entity.living.player.User;
+import org.spongepowered.api.service.user.UserStorageService;
 
 public final class PlayerUtils {
 	
+	private static UserStorageService service = null;
+	
 	private PlayerUtils() {}
 	
-	public static String getName(UUID id) {		
-		Optional<GameProfile> profile = Sponge.getServer().getGameProfileManager().getCache().getById(id);
-		if (!profile.isPresent())
-			return null;
-		return profile.get().getName().get();
+	public static String getName(UUID id) {				
+		Optional<Player> p = Sponge.getServer().getPlayer(id);
+		if (p.isPresent())
+			return p.get().getName();
+		
+		if (service == null)
+			service = Sponge.getServiceManager().provide(UserStorageService.class).get();
+		
+		Optional<User> u = service.get(id);
+		if (u.isPresent())
+			return u.get().getName();
+		return null;
 	}
 	
 }
